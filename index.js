ad500b0bb469f0780ebd5854cfc405d26d02582f
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-07-30 20:34:39 +0700

    add file deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { getMyRank, callRequest } = require('./utils/math')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
