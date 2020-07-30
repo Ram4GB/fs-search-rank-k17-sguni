5393399b712463b438dd3bd4822b421fc530693e
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-07-31 01:45:24 +0700

    update v2 finally

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
-const { getMyRank, callRequest } = require('./utils/math')
-
+const { getMyRank, callRequest } = require("./utils/math");
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
@@ -10,43 +10,133 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static(__dirname + "/public"));
 
+const getListFaculty = (req, res, next) => {
+  try {
+    let data = fs.readFileSync("./data/faculty.json");
+    data = JSON.parse(data);
+    req.faculty_array_server = data;
+    next();
+  } catch (error) {
+    req.faculty_array_server = [];
+    next();
+  }
+};
+
+app.use(getListFaculty);
 
 // Render Homepage
 app.get("/", (req, res) => {
-  res.render("index", { success: null, data: null, error: null, id: '', faculty: 'cntt_k17' });
+  res.render("index", {
+    success: null,
+    data: null,
+    error: null,
+    id: "",
+    faculty: "cntt_k17",
+    faculty_array_server: req.faculty_array_server,
+  });
 });
 
 // Process a request send to the server
 app.post("/", (req, res) => {
-
-  console.log(req.body)
-
   let { id } = req.body;
   if (!id)
     return res.render("index", {
       success: false,
       error: "Mời bạn nhập ID",
       data: null,
-      id: '', faculty: 'cntt_k17'
+      id: "",
+      faculty: "cntt_k17",
+      faculty_array_server: req.faculty_array_server,
     });
 
   if (!req.body.faculty)
-    return res.render("index", { success: false, error: "Sai mã khoa rồi", id: '', faculty: 'cntt_k17' });
+    return res.render("index", {
+      success: false,
+      error: "Sai mã khoa rồi",
+      id: "",
+      faculty: "cntt_k17",
+      faculty_array_server: req.faculty_array_server,
+    });
   if (!req.body.id)
-    return res.render("index", { success: false, error: "Bạn vui lòng nhập MSV", id: '', faculty: req.body.faculty });
+    return res.render("index", {
+      success: false,
+      error: "Bạn vui lòng nhập MSV",
+      id: "",
+      faculty: req.body.faculty,
+      faculty_array_server: req.faculty_array_server,
+    });
 
   let data = getMyRank(id, req.body.faculty);
 
-  if (!data) return res.render("index", { success: false, error: "Không tìm thấy MSV", id: '', faculty: 'cntt_k17' });
-  if (data) return res.render("index", { success: true, data, error: null, id: req.body.id, faculty: req.body.faculty });
+  if (!data)
+    return res.render("index", {
+      success: false,
+      error: "Không tìm thấy MSV",
+      id: "",
+      faculty: "cntt_k17",
+      faculty_array_server: req.faculty_array_server,
+    });
+  if (data)
+    return res.render("index", {
+      success: true,
+      data,
+      error: null,
+      id: req.body.id,
+      faculty: req.body.faculty,
+      faculty_array_server: req.faculty_array_server,
+    });
 });
 
 // Warning: Crawl a data again
 app.get("/refresh-data", async (req, res) => {
-  await callRequest(3117410000, 310);
+  const { username, password, firstId, total, name, value } = req.query;
+
+  if (!username || !password || !firstId || !total || !name || !value)
+    return res.send({
+      success: false,
+      data:
+        "username, password, firstId, total, value: cntt_k17_hk[x: number]_[year: number], name: Công nghệ thông tin K17 HK2",
+      link:
+        "refresh-data?username=123&password=123&firstId=3117410027&name=cntt_k17_hk3_2019&value=Cong%20nghe%20thong%20tin%20k17%20hk%203&3&total=3",
+    });
+
+  if (username !== "admin" && password != "admin")
+    return res.send({
+      success: false,
+      data: "Opssss, Username or password not match",
+    });
+
+  if (isNaN(firstId))
+    return res.send({
+      success: false,
+      data: "firstId must be a number",
+    });
+
+  if (isNaN(total))
+    return res.send({
+      success: false,
+      data: "total must be a number",
+    });
+
+  // create new file in data
+  await callRequest(parseInt(firstId), parseInt(total), value);
+
+  // create new faculty
+  try {
+    let data = fs.readFileSync("./data/faculty.json");
+    data = JSON.parse(data);
+    data.push({ name, value });
+    fs.writeFileSync('./data/faculty.json', JSON.stringify(data))
+  } catch (error) {
+    return res.send({
+      success: false,
+      data: "Server error",
+    });
+  }
+
   res.send({
     success: true,
-    data: "Update new data"
+    data: "Update new data",
   });
 });
 
@@ -54,11 +144,11 @@ app.listen(port, () => {
   console.log(`Server open port ${port}`);
 });
 
-// callRequest(3117410001, 312);
+// callRequest(3117410001, 312); // cntt k17 hk2 2020
 
-// callRequest(3117320001, 403);
+// callRequest(3117320001, 403); // tài chính kế toán k17 hk2 2020
+
+// callRequest(// 3117150092, 99); // gdth k17 hk2 2020
 
 // console.log(getMyRank(3.17));
 
-
-// 3117320001 
--- utils/math.js
+++ utils/math.js
@@ -1,59 +1,107 @@
-
 const fs = require("fs");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
+const _ = require('lodash') 
 
 module.exports = {
   getMyRank: (id, faculty) => {
-    let buffer
+    let buffer;
 
-    if(!faculty) return null
-    if(!id) return null
+    if (!faculty) return null;
+    if (!id) return null;
 
-    buffer = fs.readFileSync(`./data/${faculty}.txt`);
+    buffer = fs.readFileSync(`./data/${faculty}.json`);
 
     let array = JSON.parse(buffer);
     array = array
       .sort(
-        (a, b) => parseFloat(a.point).toFixed(2) - parseFloat(b.point).toFixed(2)
+        (a, b) =>
+          parseFloat(a.point).toFixed(2) - parseFloat(b.point).toFixed(2)
       )
       .reverse();
+
     // get object
-    let objectInArray = array.filter(element => {
-      return parseInt(id) === element.id;
+    let objectInArray = array.find((element) => {
+      return parseInt(id) === parseInt(element.id);
     });
+
     // get rank
-    let rank = array.findIndex(element => {
-      return parseInt(id) === element.id;
-    });
-    if (objectInArray.length == 1)
-      return { ...objectInArray[0], rank: rank + 1, total: array.length };
+    let rank = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (parseInt(id) === parseInt(array[i].id)) {
+        rank = i;
+        break;
+      }
+    }
+
+    let index = array.findIndex((a) => a.point === objectInArray.point)
+    let count = [];
+    for(let i = index ; i < array.length; i++) {
+      if(array[i].point != objectInArray.point) break;
+      else {
+        if(parseInt(array[i].id) !== parseInt(id)) count.push(array[i])
+      }
+    }
+
+    if (objectInArray)
+      return { ...objectInArray, rank: rank + 1, total: array.length, count };
     return null;
   },
-  callRequest: async (firstID, totalStudent, fileName = null) => {
+  callRequest: async (firstID, totalStudent, fileName) => {
     const total = firstID + totalStudent - 1;
+    console.log(total);
+
     let array = [];
     try {
       for (let i = firstID; i <= total; i++) {
         await rp({
           url: `http://thongtindaotao.sgu.edu.vn/Default.aspx?page=xemdiemthi&id=${i}`,
-          method: "GET"
-        }).then(body => {
-          let $ = cheerio.load(body);
-          let point = $(".row-diemTK .Label")
-            .eq(3)
-            .text();
-          if (point) array.push({ id: i, point });
-          else array.push({ id: i, point: "0.00" });
-          console.log(`${i} :`, point);
-        }).catch((e)=> console.log(i, e));
+          method: "GET",
+        })
+          .then((body) => {
+            let $ = cheerio.load(body);
+
+            // điểm học kì này
+            let point = $(".row-diemTK .Label").eq(3).text();
+
+            let point_contribution_lv10 = $(".row-diemTK .Label").eq(1).text();
+
+            let point_contribution_lv4 = $(".row-diemTK .Label").eq(7).text();
+
+            let session = $(".row-diemTK .Label").eq(11).text();
+
+            if (point)
+              array.push({
+                id: i,
+                point,
+                point_contribution_lv10,
+                point_contribution_lv4,
+                session,
+              });
+            else
+              array.push({
+                id: i,
+                point: "0.00",
+                point_contribution_lv10: "0.00",
+                point_contribution_lv4: "0.00",
+                session: 0,
+              });
+            console.log(`${i} :`, point);
+          })
+          .catch((e) => console.log(i, e));
       }
-      fs.writeFile(fileName ? `/data/${fileName}.txt` : `data/${new Date().getFullYear()}.txt`, JSON.stringify(array), function (err) {
-        if (err) console.log(err);
-        console.log("Write file successfully");
-      });
+      fs.writeFile(
+        fileName
+          ? `./data/${fileName}.json`
+          : `./data/${new Date().getFullYear()}.json`,
+        JSON.stringify(array),
+        function (err) {
+          if (err) console.log(err);
+          console.log("Write file successfully");
+        }
+      );
     } catch (error) {
       console.log(error);
     }
-  }
-}
+  },
+};
